0e2c87b5611f5b98af405f53e00eec88f1874b0e
Diego Rodrigues <diego17ar@hotmail.com>
2022-06-19 15:33:38 -0300

    Atualização do codigo, pois o mesmo não estava montando a sala

--- Cinema.py
+++ Cinema.py
@@ -1,3 +1,4 @@
+
 #Inicio do Codigo
 quantidade_assentos = int(input('Informe a quantidade de assentos: '))
 assentos = ['-']*quantidade_assentos
@@ -5,16 +6,16 @@ local=0
 
 #Dicionario que direciona as colunas de strings para inteiros
 dicionario = {
-    'A' : 1,
-    'B' : 2,
-    'C' : 3,
-    'D' : 4,
-    'E' : 5,
-    'F' : 6,
-    'G' : 7,
-    'H' : 8,
-    'I' : 9,
-    'J' : 10,
+    'a' : 1,
+    'b' : 2,
+    'c' : 3,
+    'd' : 4,
+    'e' : 5,
+    'f' : 6,
+    'g' : 7,
+    'h' : 8,
+    'i' : 9,
+    'j' : 10,
 }
 
 
@@ -35,22 +36,19 @@ while(local < len(assentos)):
             print(NumeroFila, '\n')
             NumeroFila+=1
     
-    #Escolha dos lugares
-    
+        #Escolha dos lugares
         fila = int(input('\nDigite a fila desejada: '))
         coluna = input('Digite a coluna desejada: ')
-        coluna = coluna.upper().strip()
-    
+        coluna = coluna.strip()
 
         lugarNoCinema = (fila-1)*10+(dicionario[coluna])
 
-    #Verificação dos lugares ocupados.
-        if(assentos[lugarNoCinema-1] == 'x'):
-            local-=1
-            print('A CADEIRA ESTÁ OCUPADA! ESCOLHA OUTRO LUGAR: ')
-        local+=1
+#Verificação dos lugares ocupados.
+    if(assentos[lugarNoCinema-1] == 'x'):
+        local-=1
+        print('A CADEIRA ESTÁ OCUPADA! ESCOLHA OUTRO LUGAR: ')
+    local+=1
 
-    #Finalização do Loop após todas as cadeiras estarem ocupadas.
-        assentos[lugarNoCinema-1] = 'x'
-    
+#Finalização do Loop após todas as cadeiras estarem ocupadas.
+    assentos[lugarNoCinema-1] = 'x'
 print('\t\t-- CINEMA LOTADO -- \n\n A SESSÃO VAI COMEÇAR JÁ JÁ .. TENHA UM BOM FILME! \n')
